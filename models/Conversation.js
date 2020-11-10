a34cf63e72bd3a4153fcd3eb36ce031b31159be0
peacebonez <kmpariso12@gmail.com>
2020-11-09 21:17:20 -0500

    add models directory

---
+++ models/Conversation.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ConversationSchema = new Schema({
+  members: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users", //references the users model
+      },
+    },
+  ],
+  messages: [
+    {
+      messageBody: {
+        fromUser: {
+          type: Schema.Types.ObjectId,
+          ref: "users", //references the users model
+        },
+        text: {
+          type: String,
+        },
+        createdAt: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    },
+  ],
+});
+
+module.exports = User = mongoose.model("conversation", ConversationSchema);
---
+++ models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+//create key for user avatar?
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  primaryLanguage: {
+    type: String,
+    required: true,
+  },
+  contacts: [this], // Self-referencing UserSchema.  not sure if this will work.
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = User = mongoose.model("user", UserSchema);
