3383c5085f874d1f481150681a6fd7b7a26b4be2
peacebonez <kmpariso12@gmail.com>
2020-11-12 17:25:03 -0500

    restructure user-actions to be in same file as userReducer

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,21 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useReducer } from "react";
 import { MuiThemeProvider } from "@material-ui/core";
 import { BrowserRouter, Route } from "react-router-dom";
 import Invitation from "./components/Invitation";
 import { theme } from "./themes/theme";
 import { UserContext } from "./userContext";
+import userReducer from "./reducers/userReducer";
 import Messenger from "./components/Messenger";
 
 function App() {
-  const [user, setUser] = useState(null);
+  const [user, dispatch] = useReducer(userReducer, {});
   useEffect(() => {
     //load user, check if JWT token active
   }, []);
 
   return (
     <MuiThemeProvider theme={theme}>
-      <UserContext.Provider value={{ user, setUser }}>
+      <UserContext.Provider value={user}>
         <BrowserRouter>
           <Route path="/" exact>
             <Messenger />
--- client/src/actions/user-actions.js
+++ client/src/actions/user-actions.js
@@ -7,9 +7,10 @@ const config = {
 };
 
 //Should this also create an invite in the mongoDB?
-export const sendInvite = async (userId, emailList) => {
+export const sendInvite = (userId, emailList) => async (dispatch) => {
   try {
     await axios.post(`/user/${userId}/invitation/send`, { emailList }, config);
+    dispatch({ type: "SEND_INVITE" });
   } catch (err) {
     console.error(err);
   }
