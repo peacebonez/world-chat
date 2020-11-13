10ebcb7f215ee032ccae03c40afe02ab86e6479b
peacebonez <kmpariso12@gmail.com>
2020-11-13 12:54:52 -0500

    updated test used ID

--- client/src/components/Invitation.js
+++ client/src/components/Invitation.js
@@ -89,7 +89,7 @@ export default function FormDialog() {
     handleClose();
 
     //need to grab current user id and email
-    sendInvite("5fad63358f96786e507a0b74", emailList);
+    sendInvite("5fadeb4e66d8372cd6d05d89", emailList);
     //add createInvite function
   };
 
