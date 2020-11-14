191764d51ec29ee14145af3c45353d094bc45771
peacebonez <kmpariso12@gmail.com>
2020-11-13 20:19:04 -0500

    improved approve reject routes

--- routes/invitation.js
+++ routes/invitation.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Invitation = require("../models/Invitation");
+const User = require("../models/User");
 
 //PUT request to approve invitation
 router.put("/:id/approve", async (req, res) => {
@@ -10,15 +11,19 @@ router.put("/:id/approve", async (req, res) => {
     if (!invitation) {
       res.status(404).send("Invitation not found");
     }
-    if (invitation.status !== "pending") {
+
+    //invitation
+
+    if (invitation.status === "pending") {
+      invitation.status = "approved";
+    } else {
       res
         .status(400)
         .json({ msg: `Invitation status already ${invitation.status}` });
     }
 
-    invitation.status = "approved";
-
     await invitation.save();
+    res.status(200).json({ invitation });
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server Error");
@@ -34,17 +39,18 @@ router.put("/:id/reject", async (req, res) => {
       res.status(404).send("Invitation not found");
     }
 
-    if (invitation.status !== "pending") {
+    //invitation
+
+    if (invitation.status === "pending") {
+      invitation.status = "rejected";
+    } else {
       res
         .status(400)
         .json({ msg: `Invitation status already ${invitation.status}` });
     }
 
-    invitation.status = "rejected";
-
     await invitation.save();
-
-    res.json(invitation);
+    res.status(200).json({ invitation });
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server Error");
