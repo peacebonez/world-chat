dd68a2297ca26d00b7952cab247d07ea95a4b1ae
peacebonez <kmpariso12@gmail.com>
2020-11-16 18:35:55 -0500

    merge with dev

---
+++ models/index.js
@@ -0,0 +1,9 @@
+const User = require('./User');
+const Invitation = require('./Invitation')
+const Conversation = require('./Conversation')
+
+module.exports = {
+    User,
+    Invitation,
+    Conversation
+}
