61ed03b6d874aa26509b28090def1a3f132154e9
peacebonez <kmpariso12@gmail.com>
2020-11-16 22:28:08 -0500

    UI improvements with sidebar search bar

--- client/src/components/SideBarSearch.js
+++ client/src/components/SideBarSearch.js
@@ -5,12 +5,19 @@ import { makeStyles } from '@material-ui/core/styles';
 import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
 
 const useStyles = makeStyles((theme) => ({
-  chatInput: {
-    width: '90%',
+  wrapper: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    marginLeft: theme.spacing(5),
+  },
+  searchInput: {
+    width: '80%',
     marginBottom: 20,
-    textAlign: 'center',
+    background: '#E9EEF9',
     '& input': {
-      background: theme.palette.primary.gray,
+      background: '#E9EEF9',
     },
   },
   noStyleBtn: {
@@ -47,34 +54,37 @@ const SideBarSearch = ({ setChatShown, setContactsShown, setInvitesShown }) => {
   };
 
   return (
-    <div>
-      <button
-        className={classes.noStyleBtn}
-        onClick={(e) => showComponent(e.target.innerHTML)}
-      >
-        <Typography display="inline" variant="h6">
-          Chats
-        </Typography>
-      </button>
-      <button
-        className={classes.noStyleBtn}
-        onClick={(e) => showComponent(e.target.innerHTML)}
-      >
-        <Typography display="inline" variant="h6">
-          Contacts
-        </Typography>
-      </button>
-      <button
-        className={classes.noStyleBtn}
-        onClick={(e) => showComponent(e.target.innerHTML)}
-      >
-        <Typography display="inline" variant="h6">
-          Invites
-        </Typography>
-      </button>
-      <div className={classes.chatInput}>
+    <div className={classes.wrapper}>
+      <div>
+        <button
+          className={classes.noStyleBtn}
+          onClick={(e) => showComponent(e.target.innerHTML)}
+        >
+          <Typography display="inline" variant="h6">
+            Chats
+          </Typography>
+        </button>
+        <button
+          className={classes.noStyleBtn}
+          onClick={(e) => showComponent(e.target.innerHTML)}
+        >
+          <Typography display="inline" variant="h6">
+            Contacts
+          </Typography>
+        </button>
+        <button
+          className={classes.noStyleBtn}
+          onClick={(e) => showComponent(e.target.innerHTML)}
+        >
+          <Typography display="inline" variant="h6">
+            Invites
+          </Typography>
+        </button>
+      </div>
+      <div>
         <form noValidate autoComplete="off">
           <TextField
+            className={classes.searchInput}
             variant="outlined"
             fullWidth
             autoFocus
