86e22b031daf2d9a8bcd804f6f65214a2801aaf6
peacebonez <kmpariso12@gmail.com>
2020-11-18 11:56:10 -0500

    ready for review

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,24 +1,15 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-// We need a middleware function that can:
-
-// read the jwt token from the browser cookies
-// decode that token and find the corresponding user
-// pass on to the route function which user is authenticated
-
 module.exports = (req, res, next) => {
   const token = req.header('cookie');
-  console.log('token:', token);
 
   if (!token) {
     return res.status(401).json({ msg: 'Authorization Denied' });
   }
 
   try {
-    //function makes it into the try block and crashes at const decoded
     const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-    console.log('decoded:', decoded);
     req.user = decoded;
     next();
   } catch (err) {
