938cd763db853afa2ee6231fd286ea400ec9391d
peacebonez <kmpariso12@gmail.com>
2020-11-23 20:44:02 -0500

    deleted line that crashes backend in app.js

--- app.js
+++ app.js
@@ -43,7 +43,6 @@ app.use(express.static(join(__dirname, 'public')));
 // app.use("/", require("./routes/index"));
 app.use('/user', require('./routes/user'));
 app.use('/invitation', require('./routes/invitation'));
-app.use('/conversation', require('./routes/conversation'));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
