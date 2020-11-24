b07cf16ba4b08f212b30e131df06ef9d082dd09e
Bonnie Li <bonnie@hatchways.io>
2020-11-24 15:10:50 -0500

    No need to reach into user anymore

--- middleware/auth.js
+++ middleware/auth.js
@@ -12,7 +12,7 @@ module.exports = (req, res, next) => {
   try {
     const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
-    req.user = decoded.user;
+    req.user = decoded;
     next();
   } catch (err) {
     console.error(err);
