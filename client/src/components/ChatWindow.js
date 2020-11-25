51a4569ec3453177d487939620e07d45860076cc
Evan Yang <evanyang@pop-os.localdomain>
2020-11-25 12:04:27 -0800

    commit

--- client/src/components/ChatWindow.js
+++ client/src/components/ChatWindow.js
@@ -16,39 +16,36 @@ const useStyles = makeStyles((theme) => ({
     alignItems: 'center',
     justifyContent: 'space-between',
   },
-  chatUnit: {
-  
-  },
-  chatUnitYours: {
-    
-  },
+  chatUnit: {},
+  chatUnitYours: {},
   bubble: {
-    background: 'linear-gradient(0deg, rgba(26,114,183,1) 0%, rgba(14,172,117,1) 100%)',
+    background:
+      'linear-gradient(0deg, rgba(26,114,183,1) 0%, rgba(14,172,117,1) 100%)',
     maxWidth: '18rem',
     wordWrap: 'break-word',
-    borderRadius: '0px 15px 15px 15px'
+    borderRadius: '0px 15px 15px 15px',
   },
   bubbleYours: {
-    background: 'linear-gradient(0deg, rgba(26,114,183,1) 0%, rgba(14,172,117,1) 100%)',
+    background:
+      'linear-gradient(0deg, rgba(26,114,183,1) 0%, rgba(14,172,117,1) 100%)',
     maxWidth: '18rem',
     wordWrap: 'break-word',
-    borderRadius: '15px 15px 0px 15px'
+    borderRadius: '15px 15px 0px 15px',
   },
   textInBubble: {
     color: 'white',
-    padding: '1em 1em 1em 1em'
-  }
+    padding: '10px 10px 10px 10px',
+  },
 }));
 
 const ChatWindow = () => {
   const classes = useStyles();
-  console.log(useContext(UserContext));
   const { socket, userState } = useContext(UserContext);
   const [chat, setChat] = useState([]); // an array of "data's"
 
   useEffect(() => {
     socket.on('connect', () => {
-      socket.emit('join', '123' ); // replace 123 with conversation id
+      socket.emit('join', '123'); // replace 123 with conversation id
     });
 
     socket.on('roomJoined', (room) => {
@@ -64,21 +61,21 @@ const ChatWindow = () => {
   return (
     <div className={classes.chatWindow}>
       <ul>
-        {chat.map((datum, index) => {   
+        {chat.map((datum, index) => {
           let yours = datum.email === userState.user.email;
-          console.log(datum.email, userState.user.email);
-          console.log(yours);
           return (
-            <section key={index} className={yours ? classes.chatUnitYours : classes.chatUnit}>
-              <Typography variant="subtitle2">{datum.moreData.userName} {datum.createdOn.hour}:{datum.createdOn.minute}</Typography>
-              <ChatBubble message={datum.message} yours={yours}/>
+            <section
+              key={index}
+              className={yours ? classes.chatUnitYours : classes.chatUnit}
+            >
+              <Typography variant="subtitle2">
+                {yours ? '' : datum.moreData.userName} {datum.createdOn.hour}:
+                {datum.createdOn.minute}
+              </Typography>
+              <ChatBubble message={datum.message} yours={yours} />
             </section>
-            // <li key={datum.createdOn}>
-            //   {datum.message} by {datum.email} on {datum.createdOn}
-            // </li>
           );
-        }
-        )}
+        })}
       </ul>
       <ChatInput />
     </div>
@@ -90,9 +87,11 @@ const ChatBubble = (props) => {
 
   return (
     <section className={props.yours ? classes.bubbleYours : classes.bubble}>
-      <Typography variant="subtitle1" className={classes.textInBubble}>{props.message}</Typography>
+      <Typography variant="subtitle1" className={classes.textInBubble}>
+        {props.message}
+      </Typography>
     </section>
-  )
-}
+  );
+};
 
 export default ChatWindow;
