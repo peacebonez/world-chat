a649c3c9dfc10b68416739c577bdc9eebc38c884
Evan Yang <evanyang@pop-os.localdomain>
2020-11-27 17:01:04 -0800

    google translate api

---
+++ functions/translate.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+
+const projectId = process.env.GOOGLE_PROJECT_ID;
+
+const { Translate } = require('@google-cloud/translate').v2;
+const translate = new Translate({ projectId });
+
+export const translateText = async (text, language) => {
+  const [translation] = await translate.translate(text, language);
+  return translation;
+};
